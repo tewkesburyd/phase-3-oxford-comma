56f698f10a260628588024f5952eef58bf634ffd
David Tewkesbury <tewkesburyd@hotmail.com>
2022-12-13 20:31:34 -0700

    commit tree 6c2d9600454251a9482f24c3b92a2ecf53572dbe

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    case
+        when array.length == 1 then array[0] 
+        when array.length == 2 then array[0] + ' and ' + array[1]
+        when array.length == 3 then array[0] +', '+ array[1] + ', and ' + array[2]
+        when array.length > 3 
+            array[-1] = "and #{array[-1]}"
+            array.join(", ")
+    end
 end
